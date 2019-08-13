13a45af1057756e1fd07955381776bb98591b9ba
huihuiyo <752710151@qq.com>
2019-08-13 14:09:25 +0800

    coderwars 整数每位相加 迭代 直到 取一位

---
+++ self_test/coderwars/digital_root/index.js
@@ -0,0 +1,29 @@
+function digital_root(n) {
+	// 自身迭代
+// 	var arr = (n + '').split('');
+// 	while(arr.length != 1) {
+// 		arr = addFunc(arr);
+// 	}
+// 	return parseInt(arr[0]);
+// }
+
+// function addFunc(arr) {
+// 	var res = 0;
+// 	arr.forEach(item => {
+// 		res += parseInt(item);
+// 	});
+// 	return (res + '').split('');
+	
+	// 数学方法解题
+	// return (n - 1) % 9 + 1;
+
+	// 递归 + 数组 reduce 方法解题
+	if (n < 10) return n;
+	return digital_root(n.toString().split('').reduce((pre, next) => {
+		return pre + +next;
+	}, 0));
+}
+
+	
+
+console.log(digital_root(888))
