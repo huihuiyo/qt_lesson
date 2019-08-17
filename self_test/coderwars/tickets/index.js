bb99d76fbefd3e306804a899c18b218cb4f505ca
huihuiyo <752710151@qq.com>
2019-08-17 11:36:37 +0800

    coderwars 找零

--- self_test/coderwars/tickets/index.js
+++ self_test/coderwars/tickets/index.js
@@ -1,34 +1,63 @@
 function tickets(peopleInLine){
 	// ...
-	let tempArr = peopleInLine[0];
-	peopleInLine.reduce((pre, next) => {
-		switch(next) {
-			case 25:
-				tempArr.unshift(next);
-				return tempArr;
-			case 50:
-				if (tempArr.contains(25)) {
-					tempArr.sort();
-					tempArr.shift();
-					tempArr.unshift(next)
-					console.log(tempArr);
-					return tempArr;
-				} else {
-					return 'No';
-				}
-			case 100:
-				if (tempArr.contains(25) && tempArr.contains(50)) {
-					tempArr.sort();
-					tempArr.pop();
-					tempArr.shift();
-					return tempArr;
-				} else {
-					return tempArr;
-				}
-				
-		}
+	// let tempArr = [];
+	// let result = 'YES';
+	// tempArr.unshift(peopleInLine[0]);
+	// peopleInLine.reduce((pre, next, index, arr) => {
+	// 	switch(next) {
+	// 		case 25:
+	// 			tempArr.unshift(next);
+	// 			return tempArr;
+	// 		case 50:
+	// 			if (tempArr.indexOf(25) != -1) {
+	// 				tempArr.sort();
+	// 				tempArr.shift();
+	// 				tempArr.unshift(next);
+	// 				return tempArr;
+	// 			} else {
+	// 				result = 'NO';
+	// 				arr.splice(1);
+	// 			}
+	// 		case 100:
+	// 			tempArr.sort();
+	// 			if (tempArr.indexOf(25) != -1 && tempArr.indexOf(50) != -1) {
+	// 				tempArr.pop();
+	// 				tempArr.shift();
+	// 				return tempArr;
+	// 			} else if (tempArr[2] == 25) {
+	// 				tempArr.splice(0, 3);
+	// 			} else {
+	// 				result = 'NO';
+	// 				arr.splice(1);
+	// 			}
+	// 	}
+	// })
+	// return result;
 
-	})
+	// 写好看一点
+	var judge = new JudgeFunc();
+	return peopleInLine.every(judge.change.bind(judge)) ? 'YES' : 'NO';
+}
+function JudgeFunc() {
+	this.money = {
+		25: 0,
+		50: 0
+	}
+	this.change = function(element) {
+		this.money[element]++;
+		switch (element) {
+			case 25:
+				return true;
+			case 50:
+				this.money[25]--;
+				break;
+			case 100:
+				this.money[50] ? this.money[50]-- : this.money[25] -= 2;
+				this.money[25]--;
+				break;
+		}
+		return this.money[25] >= 0;
+	}
 }
 
-console.log(tickets([25, 25, 50, 50]));
+console.log(tickets([50,100,25,25,25,100,25,25,50,100,25,25,25,100,25,25,50,100]));
