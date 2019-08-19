1d71e7fde4584a43e3fe864874fba19db9ffd020
huihuiyo <752710151@qq.com>
2019-08-19 11:41:46 +0800

    leetcode 两数之和=target 返回两数下标 map解法

--- self_test/LeetCode_algorithm/twosum/index.js
+++ self_test/LeetCode_algorithm/twosum/index.js
@@ -1,16 +1,28 @@
 var twoSum = function(nums, target) {
-    var count = 1;
-    for(var i = 0; i < nums.length; i++) {
-        for(var j = count; j < nums.length; j++){
-            if(nums[i] + nums[j] == target){
-                var result = [];
-                result.push(i);
-                result.push(j);
-                console.log(result);
-            }
-        }
-        count++;
-    }
+    // var count = 1;
+    // for(var i = 0; i < nums.length; i++) {
+    //     for(var j = count; j < nums.length; j++){
+    //         if(nums[i] + nums[j] == target){
+    //             var result = [];
+    //             result.push(i);
+    //             result.push(j);
+    //             console.log(result);
+    //         }
+    //     }
+    //     count++;
+	// }
+	var map = {};
+	for (let i = 0; i < nums.length; i++) {
+		let num = nums[i];
+		map[num] = i;
+	}
+	for (let j = 0; j < nums.length; j++) {
+		let num1 = nums[j];
+		let anotherNum = target - num1;
+		if (map[anotherNum] !== undefined && map[anotherNum] !== j) {
+			return [j, map[anotherNum]];
+		}
+	}
 };
 var nums = [2, 7, 11, 15],
     target = 26;
