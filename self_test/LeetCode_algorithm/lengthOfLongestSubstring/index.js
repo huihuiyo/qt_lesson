44e8bed392ab97a44d7e8548380226ebae703818
huihuiyo <752710151@qq.com>
2019-08-23 13:44:42 +0800

    leetcode 查字符串中最长不重复长度

--- self_test/LeetCode_algorithm/lengthOfLongestSubstring/index.js
+++ self_test/LeetCode_algorithm/lengthOfLongestSubstring/index.js
@@ -1,10 +1,34 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var lengthOfLongestSubstring = function(s) {
-    var bCount = 0;
-    var curStr = '';
-    for (let i = 0; i < s.length; i++) {
-        if(curStr[curStr.length - 1] != s[i]) {
-            bCount++;
-            curStr += s[i];
-        }
-    }
+	var currArr = s.split('');
+	var tempStr = '';
+	if(currArr.length < 2) {
+		tempStr = s;
+	}
+	if(currArr.length == 2) {
+		currArr[0] == currArr[1] 
+		? tempStr = currArr[0] 
+		: tempStr = s;
+	}
+	if(currArr.length > 2) {
+		currArr.reduce((pre, next) => {
+			if (pre.indexOf(next) == -1){
+				pre += next;
+				if(pre.length > tempStr.length)
+					tempStr = pre;
+				return pre;
+			}
+			if(pre.length > tempStr.length)
+				tempStr = pre;
+			// console.log(pre.slice(pre.indexOf(next)));
+			return pre = pre.slice(pre.indexOf(next)+1) + next;
+		});
+	}
+	console.log(tempStr)
+	return tempStr.length;
 };
+
+console.log(lengthOfLongestSubstring("nfpdmpi"));
