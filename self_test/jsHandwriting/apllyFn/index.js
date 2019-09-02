1eab533d41b757f3db156da56edaae8bfac85d17
huihuiyo <752710151@qq.com>
2019-09-02 20:30:24 +0800

    js手写apply方法

---
+++ self_test/jsHandwriting/apllyFn/index.js
@@ -0,0 +1,19 @@
+Function.prototype.applyFn = function (obj, rest) {
+	obj._fn = this;
+	let res = obj._fn(rest);
+	delete obj._fn;
+	return res;
+};
+function testApplyFn (arr) {
+	var ret = this.a;
+	arr.forEach(item => {
+		ret += item;
+	});
+	return ret;
+}
+var testObj = {
+	a: '1',
+}
+
+
+console.log(testApplyFn.applyFn(testObj, ['2', '3']))
