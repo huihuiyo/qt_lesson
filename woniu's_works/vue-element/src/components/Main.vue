8287365e91a5e5c1f5edbc3fc2f3f6b5efbe762e
huihuiyo <752710151@qq.com>
2019-09-04 17:48:52 +0800

    vue脚手架后构建登录demo

---
+++ woniu's_works/vue-element/src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main-content">
+    <!-- 头部header -->
+    <Header></Header>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+import Header from '@/common/Header'
+export default {
+  name: 'mainContent',
+  data () {
+    return {
+    }
+  },
+  components: {
+    Header: Header
+  }
+}
+</script>
