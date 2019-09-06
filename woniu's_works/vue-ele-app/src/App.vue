7c1a7777c75ead8cb42d6811041d6acd3ae656b2
huihuiyo <752710151@qq.com>
2019-09-06 17:15:18 +0800

    vue脚手架 写app项目实操

---
+++ woniu's_works/vue-ele-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <v-Header :seller="seller"></v-Header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header/Header'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  components: {
+    'v-Header': Header
+  },
+  created () {
+    this.$http.get('https://www.easy-mock.com/mock/5d721c4c9aaed00b9c3bde5e/vue-eleme-seller/vue-eleme-seller').then(res => {
+      if (res.data.errno === 0) {
+        this.seller = Object.assign({}, this.seller, res.data.data)
+      }
+    })
+  }
+}
+</script>
+
+<style>
+</style>
