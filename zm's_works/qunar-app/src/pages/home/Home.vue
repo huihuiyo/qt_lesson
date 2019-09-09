14cfaefa4f51edc7af57f90cf0d11c3ef55a0417
huihuiyo <752710151@qq.com>
2019-09-09 18:48:10 +0800

    vue搭建 去哪app

---
+++ zm's_works/qunar-app/src/pages/home/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <home-header />
+  </div>
+</template>
+
+<script>
+import HomeHeader from '../../components/Home-Head'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    'home-header': HomeHeader
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
