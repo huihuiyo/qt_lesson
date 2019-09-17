464f1b110c625652f2335bbde28aeec49dd265ed
huihuiyo <752710151@qq.com>
2019-09-17 16:33:25 +0800

    vue toast独立组件编写

---
+++ zm's_works/vue-toast-component/README.md
@@ -0,0 +1,19 @@
+## 弹出类 组件
+1. html结构
+   和页面主体内容 同级，不应该嵌套在页面里面
+
+## 面向对象
+编程范式
+解决一类问题
+
+定义一个 vue 组件 <template> <script> <style>
+实际上 是 一个构造函数
+
+## 1
+import Toast from ''
+<xxx />
+
+## 2
+手动拿到 Toast 构造函数
+vue.extend()
+生成了一个 Toast 实例
---
+++ zm's_works/vue-toast-component/src/components/toast/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="toast" v-show="visiable">
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  props: {
+    msg: {
+      type: String,
+      required: true
+    },
+    visiable: {
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style scoped>
+.toast {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  background: rgba(0, 0, 0, 0.35);
+  padding: 10px;
+  border-radius: 5px;
+  transform: translate(-50%, -50%);
+  color: #fff;
+}
+.toast-msg {
+  text-align: center;
+}
+</style>
