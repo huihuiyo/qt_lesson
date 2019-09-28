4c893a4801db3d9bde3629d67a0d124cf85f6a2a
huihuiyo <752710151@qq.com>
2019-09-28 14:12:22 +0800

    vue 全家桶 网易云音乐

--- woniu's_works/vuex-NeteaseCloudMusic/src/components/pages/search.vue
+++ woniu's_works/vuex-NeteaseCloudMusic/src/components/pages/search.vue
@@ -4,10 +4,34 @@
       <v-search-box @query="onQueryChange" ref="searchBox"></v-search-box>
     </div>
     <div class="shortcut-wrapper" ref="shortcutWrapper" v-show="!query">
-      <!-- 热门搜索 -->
-      <v-scroll>
-        
+      <v-scroll class="shortcut" ref="shortcut" :data='shortcut' :refreshDelay='refreshDelay'>
+        <div>
+          <!-- 热门搜索 -->
+          <div class="hot-key">
+            <h1 class="title">热门搜索</h1>
+            <ul>
+              <li class="item" v-for="(item, index) in hotKey" :key="index">
+                <span>{{item.first}}</span>
+              </li>
+            </ul>
+          </div>
+          <!-- 搜索历史 -->
+          <div class="search-history" v-show="searchHistory.length">
+            <h1 class="title">
+              <span class="text">搜索历史</span>
+              <span class="clear">
+                <i class="icon">&#xe612;</i>
+              </span>
+            </h1>
+            <!-- 搜索历史列表 -->
+            <v-searchList :searches='searchHistory' @select='addQuery'></v-searchList>
+          </div>
+        </div>
       </v-scroll>
+    </div>
+    <!-- 搜索结果 -->
+    <div class="search-result" v-show="query" ref="rearchResult">
+      
     </div>
   </div>
 </template>
@@ -15,24 +39,78 @@
 <script>
 import searchBox from '@/components/searchBox'
 import scroll from '@/components/scroll'
+import searchList from '@/components/searchList'
 export default {
   data () {
     return {
-
+      query: '',
+      shortcut: [],
+      refreshDelay: 1,
+      hotKey: [
+        {first: '123'},
+        {first: '456'},
+        {first: '78999999999999999999999999999'}
+      ],
+      searchHistory: [1]
     }
   },
   components: {
     'v-search-box': searchBox,
-    'v-scroll': scroll
+    'v-scroll': scroll,
+    'v-searchList': searchList
   },
   methods: {
     onQueryChange () {
+
+    },
+    addQuery () {
 
     }
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus">
+@import '../../assets/css/function.styl';
+.search
+  overflow hidden
+  &-box-wrapper
+    margin px2rem(40px)
+  .shortcut-wrapper
+    position fixed
+    top px2rem(360px)
+    bottom 0
+    width 100%
+    .shortcut
+      height 100%
+      overflow hidden
+      .hot-key
+        margin 0 px2rem(40px) px2rem(40px) px2rem(40px)
+        .title
+          margin-bottom px2rem(40px)
+          font-size 14px
+          color hsla(0, 0%, 100%, 0.5)
+        .item
+          display inline-block
+          padding px2rem(10px) px2rem(20px)
+          margin 0 px2rem(20px) px2rem(20px) 0
+          border-radius 6px
+          font-size 14px
+          color hsla(0, 0%, 100%, 0.3)
+          background #2f3054
+      .search-history
+        position relative
+        margin 0 px2rem(40px)
+        .title
+          display flex
+          align-items center
+          height px2rem(80px)
+          font-size 14px
+          color hsla(0, 0%, 100%, 0.5)
+          .text
+            flex 1
+          .clear
+            .icon
+              font-size 18px
+              color hsla(0, 0%, 100%, 0.3)
 </style>
---
+++ woniu's_works/vuex-NeteaseCloudMusic/src/components/searchList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="search-list">
+    <transition-group name="list" tag="ul">
+      <li class="search-item" v-for="item in searches" :key="item" @click="selectItem(item)">
+        <span class="text">{{item}}</span>
+        <span class="icon-box" @click.stop="deleteOne(item)">
+          <i class="icon">&#xe656;</i>
+        </span>
+      </li>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    searches: {
+      type: Array,
+      default: []
+    }
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    selecteItem(item) {
+      this.$emit('select', item)
+    },
+    deleteOne(item) {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
