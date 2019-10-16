71aa54ae411b61485e2567453d276bb3f39b41b4
huihuiyo <752710151@qq.com>
2019-10-16 18:27:42 +0800

    vuex数据规划-以shop-cart为例

---
+++ teacherY‘s_works/vue/yyx-shop/README.md
@@ -0,0 +1,12 @@
+- product cart
+  商品数据
+  数量
+  cart ？
+- 模块化后，看一眼设计稿，把核心的状态，状态的修改，添加核心的mutations、actions
+  state -> actions -> api -> mutations -> state
+  数据的流转进行逻辑设计
+- 添加商品到购物车
+  cart.state.items
+  mapActions('cart', {
+    addProductToCart
+  })
---
+++ teacherY‘s_works/vue/yyx-shop/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>Shopping Cart Example</h1>
+    <h2>Products</h2>
+    <ProductList />
+    <br>
+    <ShoppingCart />
+  </div>
+</template>
+<script>
+import ProductList from '@/components/ProductList.vue'
+import ShoppingCart from '@/components/ShoppingCart.vue'
+export default {
+  components: {
+    ProductList,
+    ShoppingCart
+  }
+}
+</script>
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+
+#nav
+  padding 30px
+  a
+    font-weight bold
+    color #2c3e50
+    &.router-link-exact-active
+      color #42b983
+</style>
