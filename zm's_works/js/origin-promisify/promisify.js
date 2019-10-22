b07ff4e622978196683051dae945f97fa48167a1
huihuiyo <752710151@qq.com>
2019-10-22 17:00:27 +0800

    手写promisify-文件流异步方法

---
+++ zm's_works/js/origin-promisify/promisify.js
@@ -0,0 +1,19 @@
+// const { promisify } = require('util');
+const fs = require('fs');
+function promisify(fn) {
+  return function (...arg) {
+    return new Promise((resolve, reject) => {
+      // fn === fs.readFile
+      fn(...arg, (err, data) => {
+        if(err) {
+          reject(err);
+        }
+        else {
+          resolve(data);
+        }
+      })
+    })
+  }
+}
+const readFile = promisify(fs.readFile);
+readFile('./a.txt', 'utf8').then(res => console.log(res))
