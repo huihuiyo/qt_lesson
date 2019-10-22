f75840dbee70fd75942497414a21ebea22ad523b
huihuiyo <752710151@qq.com>
2019-10-22 17:07:50 +0800

    vue3.0中reactivity响应式实现原理

---
+++ zm's_works/vue/vue-reactivity-module/my-reactivity/reactive.js
@@ -0,0 +1,29 @@
+import { track, trigger } from './effect.js'
+export function reactive(target) {
+  const observed = new Proxy(target, {
+    set: (target, key, receiver) => {
+      // 恢复它 “赋值” 的一个行为
+      const result = Reflect.set(target, key, receiver);
+      trigger(target, key);
+      return result;
+    },
+    get: (target, key, receiver) => {
+      // mobx 也有 effect 也有依赖收集
+      // 依赖收集
+      console.log('依赖收集');
+      const res = Reflect.get(target, key, receiver);
+
+      // {
+      //   origin: {
+      //     count: {
+      //       fn1,
+      //       fn2
+      //     }
+      //   }
+      // }
+      track(target, 'get', key);
+      return res;
+    }
+  });
+  return observed;
+}
