92b0e20d732bda7de3d4a24853c49cfaa39bbdc5
huihuiyo <752710151@qq.com>
2019-10-24 16:09:58 +0800

    jsonP 跨域问题-前端通过url传参调用后端方法-参数需要和后端方法名相同！

--- zm's_works/node/jsonp/node.js
+++ zm's_works/node/jsonp/node.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  if (req.url === '/api/books') {
+  const url = require('url').parse(req.url);
+  if (url.pathname === './api/books') {
+    const methodName = url.query && url.query.split('=')[1];
     let list = [ {book1: 'bkk1', book2: 'bkk2'} ];
-    res.end(`getBook(${JSON.stringify(list)})`);
+    res.end(`${methodName}(${JSON.stringify(list)})`);
   }
 })
 .listen(3002, () => {
