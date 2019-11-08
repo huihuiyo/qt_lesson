6d7bd437693e03e6e84ab3147d0bbb2bb7f0b459
huihuiyo <752710151@qq.com>
2019-11-08 17:06:11 +0800

    深拷贝-栈实现方式

---
+++ woniu's_works/interview/deepClone/deep-copy.js
@@ -0,0 +1,56 @@
+function cloneDeep(x) {
+  const root = {}
+
+  // 栈
+  const loopList = [
+    {
+      parent: root,
+      key: undefined,
+      data: x
+    }
+  ]
+
+  while (loopList.length) {
+    // 广度优先
+    const node = loopList.pop()
+    const parent = node.parent
+    const key = node.key
+    const data = node.data
+
+    // 初始化赋值目标， key为undefined则拷贝到父元素， 否则拷贝到子元素
+    let res = parent
+    if (typeof key !== 'undefined') {
+      res = parent[key] = {}
+
+    }
+    for (const k in data) {
+      if (data.hasOwnProperty(k)) {
+        if (typeof data[k] === 'object') {
+          // 下一次循环
+          loopList.push(
+            {
+              parent: res,
+              key: k,
+              data: data[k]
+            }
+          )
+        } else {
+          res[k] = data[k]
+        }
+        
+      }
+    }
+  }
+  return root
+}
+
+// test
+let objA = {
+  'name': 'HH',
+  'hobie': {
+    'a': 'xx'
+  }
+}
+let objB = cloneDeep(objA)
+objA.hobie.a = 'hh'
+console.log(objB)
