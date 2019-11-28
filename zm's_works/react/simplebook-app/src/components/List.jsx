8e477900b1ccff16ce03a7e2fa7c3e87ca779c92
huihuiyo <752710151@qq.com>
2019-11-28 22:02:19 +0800

    react 简书

---
+++ zm's_works/react/simplebook-app/src/components/List.jsx
@@ -0,0 +1,55 @@
+import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { ListItem, ListInfo, LoadMore } from "../pages/home/style";
+import { actionCreators } from "../pages/home/store/index";
+class List extends Component {
+  state = {  }
+  render() {
+    const { list, page } = this.props
+    return (
+      <div>
+        {
+          list.map((item, i) => {
+            return (
+              <Link key={`item${i}`} to={ '/detail/' + item.get('id') }>
+                <ListItem>
+                  <img alt='' className='pic' src={item.get('imgUrl')} />
+                  <ListInfo>
+                    <h3 className="title">
+                      { item.get('title') }
+                    </h3>
+                    <p className="desc">
+                      { item.get('desc') }
+                    </p>
+                  </ListInfo>
+                </ListItem>
+              </Link>
+            )
+          })
+        }
+        <LoadMore onClick={() => {
+          this.props.getMoreList(page)
+        }}>
+          更多文字
+        </LoadMore>
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    list: state.getIn(['home', 'articleList']),
+    page: state.getIn(['home', 'articlePage'])
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getMoreList: (page) => {
+      // action {}
+      // action Func
+      dispatch(actionCreators.getMoreList(page))
+    }
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(List);
