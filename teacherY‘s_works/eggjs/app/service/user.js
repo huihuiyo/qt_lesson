faf8fa1740432bfed793f9ea8a85b9458a284d7f
huihuiyo <752710151@qq.com>
2019-12-01 20:31:56 +0800

    eggjs sequelize 的使用

---
+++ teacherY‘s_works/eggjs/app/service/user.js
@@ -0,0 +1,27 @@
+const Service = require('egg').Service
+const uuid = require('uuid')
+const crypto = require('crypto')
+
+class UserService extends Service {
+  async register(user) {
+    // sql 及 存储业务的组织
+    const { ctx, app } = this
+    // 准备user_id
+    // user 正在成为一个对象
+    user.user_id = uuid.v4().replace(/-/g, '')
+    console.log(user.user_id)
+    // 密码不能为明文
+    user.password = crypto.createHmac('sha256', app.config.password_secret).update(user.password).digest('hex')
+    console.log(user, '--------------')
+    const userInfo = await ctx.model.User.create(user)
+    ctx.status = 200
+    ctx.body = {
+      msg: '注册成功',
+      user_id: user.user_id,
+      flag: true
+    }
+    return userInfo.dataValues
+  }
+}
+
+module.exports = UserService
